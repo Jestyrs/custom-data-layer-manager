b061c337bf6715cf04046428734256cf84208aef
Jestyrs <rhage91@Gmail.com>
2025-12-11 10:09:19 -0600

    Update defaultSchema in mock-environment.js and app.js to match production schema

--- demo/app.js
+++ demo/app.js
@@ -445,46 +445,151 @@ const app = {
         const defaultSchema = {
             type: 'object',
             properties: {
-                page: {
-                    type: 'object',
+                application: {
                     properties: {
-                        view_name: { type: 'string' },
-                        url: { type: 'string' },
+                        touchpoint: {
+                            type: 'string'
+                        },
+                        kpi: {
+                            type: 'string'
+                        },
                         assets: assetsType
                     }
                 },
-                application: {
-                    type: 'object',
+                event: {
                     properties: {
-                        touchpoint: { type: 'string' },
-                        kpi: { type: 'string' },
+                        action: {
+                            type: 'string'
+                        },
+                        assets: assetsType
+                    }
+                },
+                page: {
+                    properties: {
+                        name: {
+                            type: 'string'
+                        },
+                        query_string: {
+                            type: 'string'
+                        },
+                        hash: {
+                            type: 'string'
+                        },
+                        url: {
+                            type: 'string'
+                        },
+                        path: {
+                            type: 'string'
+                        },
+                        campaign_code: {
+                            type: 'string'
+                        },
+                        assets: assetsType,
+                        previous: {
+                            type: 'string'
+                        },
+                        views: {
+                            type: 'number'
+                        },
+                        error_page: {
+                            type: 'boolean'
+                        },
+                        home_page: {
+                            type: 'boolean'
+                        },
+                        view_name: {
+                            type: 'string'
+                        }
+                    }
+                },
+                form: {
+                    properties: {
+                        assets: assetsType,
+                        error_message: {
+                            type: 'string'
+                        },
+                        source: {
+                            type: 'string'
+                        },
+                        name: {
+                            type: 'string'
+                        }
+                    }
+                },
+                search: {
+                    properties: {
+                        term: {
+                            type: 'string'
+                        },
+                        result_count: {
+                            type: 'integer'
+                        },
+                        category: {
+                            type: 'string'
+                        },
+                        filter: {
+                            type: 'string'
+                        },
+                        position: {
+                            type: 'integer'
+                        },
                         assets: assetsType
                     }
                 },
                 user: {
-                    type: 'object',
                     properties: {
-                        id: { type: 'string' },
-                        isLoggedIn: { type: 'boolean' },
-                        assets: assetsType
-                    }
-                },
-                event: { type: 'object', properties: { action: { type: 'string' } } },
-                search: {
-                    type: 'object',
-                    properties: {
-                        term: { type: 'string' },
-                        results_count: { type: 'number' },
-                        result_position: { type: 'number' },
-                        assets: assetsType
-                    }
-                },
-                form: {
-                    type: 'object',
-                    properties: {
-                        name: { type: 'string' },
-                        step: { type: 'string' },
-                        assets: assetsType
+                        gender: {
+                            type: 'string'
+                        },
+                        authenticated: {
+                            type: 'boolean'
+                        },
+                        group_number: {
+                            type: 'string'
+                        },
+                        group_name: {
+                            type: 'string'
+                        },
+                        lob_plan_code: {
+                            type: 'string'
+                        },
+                        exchange: {
+                            type: 'string'
+                        },
+                        aso: {
+                            type: 'boolean'
+                        },
+                        custom_network: {
+                            type: 'boolean'
+                        },
+                        age: {
+                            type: 'string'
+                        },
+                        policy_active: {
+                            type: 'boolean'
+                        },
+                        pcp_selected: {
+                            type: 'boolean'
+                        },
+                        signature_service: {
+                            type: 'boolean'
+                        },
+                        smart_shopper: {
+                            type: 'boolean'
+                        },
+                        assets: assetsType,
+                        session_start: {
+                            type: 'string'
+                        },
+                        ruid: {
+                            type: 'string'
+                        },
+                        person_id: {
+                            type: 'string'
+                        },
+                        relationship: {
+                            type: 'string'
+                        }
                     }
                 }
             }
